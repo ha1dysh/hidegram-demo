1246b358222bbec91ebf8c640ad7b00f2ab48f5a
ha1dysh <olekshaid@gmail.com>
2024-03-24 11:37:38 +0200

    feat: add file type in message item

--- src/pages/chats/messageItem.tsx
+++ src/pages/chats/messageItem.tsx
@@ -3,7 +3,13 @@ import { twMerge } from "tailwind-merge";
 type Props = {
 	id: string;
 	type: string;
-	content: string;
+	content: {
+		text?: string;
+		title?: string;
+		url?: string;
+		icon?: string;
+		description?: string;
+	};
 	sender: { id: string; name: string };
 	timestamp: string;
 	status: string;
@@ -24,7 +30,7 @@ function MessageItem({
 
 	if (type === "image") {
 		return (
-			<div
+			<li
 				className={twMerge(
 					"relative max-w-[80%] md:max-w-[30%] mt-2",
 					isPreviousSameAuthor && "mt-1",
@@ -43,11 +49,58 @@ function MessageItem({
 						</span>
 					</div>
 				)}
-				<img src={content} alt="image" className="rounded-2xl" />
+				<img src={content.url} alt="image" className="rounded-2xl" />
 				<div className="absolute bottom-[6px] right-[8px] w-[37px] h-[16] bg-[#313131] opacity-70 text-center text-[11px] rounded-[5px]">
-					{new Date(timestamp).toLocaleTimeString().slice(0, -6)}
+					{timestamp.slice(11, 16)}
 				</div>
-			</div>
+			</li>
+		);
+	} else if (type === "file") {
+		return (
+			<li
+				className={twMerge(
+					"max-w-[calc(75%)] md:max-w-[calc(45%)] mt-2 md:bg-darkGray py-3 pl-3 pr-2",
+					"rounded-t-[16px] md:rounded-t-[14px] ",
+					isPreviousSameAuthor && "mt-1",
+					// prettier-ignore
+					isBob
+					? twMerge("self-end rounded-bl-[16px] rounded-br-[8px] md:rounded-bl-[14px] md:rounded-br-none bg-blue",
+							isPreviousSameAuthor && "rounded-br-[8px] md:rounded-[14px]")
+					: twMerge("self-start rounded-bl-[8px] rounded-br-[16px] md:rounded-bl-none md:rounded-br-[14px] bg-darkGray",
+							isPreviousSameAuthor && "rounded-tl-[8px] md:rounded-[14px]" )
+				)}
+			>
+				{(isMoreThanTwoAuthors && isPreviousSameAuthor) || (
+					<p className={twMerge("text-[#EDA0A0]", isBob && "hidden")}>
+						{sender.name}
+					</p>
+				)}
+				<div className="flex items-center gap-3">
+					<img
+						src={content.icon}
+						alt="file icon"
+						className="w-[40px] h-[52px]"
+					/>
+					<div className="">
+						<p className="text-sm font-medium line-clamp-1">
+							{content.title}
+						</p>
+						<p className="text-[13px] text-[#E9E9EB]">
+							{content.description}
+						</p>
+						<p
+							className={twMerge(
+								"relative top-[10px] float-end text-[11px] md:text-xs",
+								isBob
+									? "text-[#D7D7D7] md:text-gray"
+									: "text-gray"
+							)}
+						>
+							{timestamp.slice(11, 16)}
+						</p>
+					</div>
+				</div>
+			</li>
 		);
 	}
 
@@ -71,14 +124,14 @@ function MessageItem({
 				</p>
 			)}
 			<p className="">
-				<span>{content}</span>
+				<span>{content.text}</span>
 				<span
 					className={twMerge(
 						"relative top-[10px] pl-2 float-end text-[11px] md:text-xs pb-1.5",
 						isBob ? "text-[#D7D7D7] md:text-gray" : "text-gray"
 					)}
 				>
-					{new Date(timestamp).toLocaleTimeString().slice(0, -6)}
+					{timestamp.slice(11, 16)}
 				</span>
 			</p>
 		</li>
