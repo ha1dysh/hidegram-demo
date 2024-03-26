79f0e771cd05f1f505221aba44722aef78c04fa5
ha1dysh <olekshaid@gmail.com>
2024-03-26 16:35:57 +0200

    chore: modal agreement

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,16 +1,26 @@
-import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import Agreement from "@/components/agreement";
 
 export default function Layout() {
+	const [isShownAgreement, setIsShownAgreement] = useState(() => true);
 	const navigate = useNavigate();
+	const { pathname } = useLocation();
 
 	useEffect(() => {
 		navigate("/chats");
 	}, [navigate]);
 
 	return (
-		<div className="h-full text-white bg-black md:flex">
+		<div
+			id="layout"
+			className="relative h-full text-white bg-black md:flex"
+		>
 			<Outlet />
+
+			{isShownAgreement && pathname === "/access/signup" && (
+				<Agreement setIsShownAgreement={setIsShownAgreement} />
+			)}
 		</div>
 	);
 }
