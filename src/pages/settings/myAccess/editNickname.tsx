2b72fa234943d050d9f7e9e8717f28f493e891ee
ha1dysh <olekshaid@gmail.com>
2024-04-03 22:43:52 +0300

    style: edit nickname divider

--- src/pages/settings/myAccess/editNickname.tsx
+++ src/pages/settings/myAccess/editNickname.tsx
@@ -14,17 +14,19 @@ function EditNickname() {
 			</Header>
 
 			<div className="max-w-2xl mx-auto mt-[25px] md:bg-darkGray md:rounded-[10px]">
+				<Divider full className="ml-0 md:hidden md:ml-4" />
 				<Input
 					type="text"
 					placeholder="Text Field"
 					className="h-[58px] md:h-[50px] px-[14px] md:rounded-[10px] bg-black md:bg-darkGray text-[20px] md:text-[17px] border border-borderColor border-x-0 md:border-0 placeholder:text-[20px] md:placeholder:text-[17px]"
 				/>
-				<Divider className="hidden md:block" full />
+				<Divider full className="ml-0 md:ml-4" />
 				<Input
 					type="text"
 					placeholder="Last Name"
 					className="h-[58px] md:h-[50px] px-[14px] md:rounded-[10px] bg-black md:bg-darkGray text-[20px] md:text-[17px] border border-borderColor border-t-0 border-x-0 md:border-0 md:placeholder:text-[17px]"
 				/>
+				<Divider full className="ml-0 md:hidden md:ml-4" />
 			</div>
 		</>
 	);
